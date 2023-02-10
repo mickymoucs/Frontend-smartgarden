44786591b177ccb2bfcf3ce141f6f6cd2e459f17
thanidacwn <92778351+thanidacwn@users.noreply.github.com>
2023-02-10 11:53:28 +0700

    post: update_garden

--- main.py
+++ main.py
@@ -40,6 +40,14 @@ class BuzzerSunroof(BaseModel):
     buzzer: bool
     sunroof: bool
 
+class Data(BaseModel):
+    moist_value: float
+    moist_default: float
+    sprinkle1: Sprinkle
+    sprinkle2: Sprinkle
+    buzzer: bool
+    sunroof: bool
+
 
 def moisture_to_percentage(moisture):
     percentage = round((moisture/1023)*100,2)
@@ -57,12 +65,38 @@ def read_root():
 
 @app.get("/garden/all")
 def read_all_garden():
-    return list(collection.find())
+    data = collection.find({}, {"_id": False})
+
 
 @app.post("/garden/update")
-def update_garden():
-    collection.update_one({"name": "moisture"}, {"$set": {"is_active": False}})
-    return {"message": "All garden is inactive."}
+def update_garden(data: Body()):
+    for item in collection.find({}, {"_id": False}):
+        name = item["name"]
+        if name == "moisture":
+            is_auto = item.get("is_auto", False)
+            is_active = item.get("is_active", False)
+            if is_auto or is_active:
+                collection.update_one({"": "moisture"}, percentage_to_moisture({"$set": {"moisture_default": int(moisture_default)}}))
+
+        elif name == "sprinkle":
+            is_auto = item["is_auto"]
+            is_active = item["is_active"]
+            if is_auto and is_active:
+                collection.update_one({"": "moisture"}, percentage_to_moisture({"$set": {"moisture_default": int(moisture_default)}}))
+
+        elif name == "buzzer-sunroof":
+            buzzer = item["buzzer"]
+            sunroof = item["sunroof"]
+            update_dict = {}
+            if buzzer:
+                update_dict["buzzer"] = True
+                update_dict["sunroof"] = False
+            elif sunroof:
+                update_dict["sunroof"] = True
+            collection.update_one({"":"buzzer-sunroof"}, {"$set": update_dict})
+
+    return {"message": "Garden is already update."}
+
 
 @app.get("/garden/sprinkle")
 def sprinkle():
@@ -87,6 +121,3 @@ def update_moisture(name: str, value: int):
 @app.post("/update/buzzer-sunroof")
 def update_buzzer(name: str, value: int):
     pass
-
-
-
