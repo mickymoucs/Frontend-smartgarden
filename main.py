5aa3a94b1532a3ad3d2eebfdf68645caaecda839
Napasakorn Boonkerd <89629829+Savetang19@users.noreply.github.com>
2023-02-10 13:50:20 +0700

    fix spelling

--- main.py
+++ main.py
@@ -42,6 +42,14 @@ class BuzzerSunroof(BaseModel):
     buzzer: bool
     sunroof: bool
 
+class Data(BaseModel):
+    moist_value: int
+    moist_default: int
+    sprinkle_1: Sprinkle
+    sprinkle_2: Sprinkle
+    buzzer: bool
+    sunroof: bool
+
 
 def moisture_to_percentage(moisture):
     percentage = int((moisture/1023)*100)
@@ -82,9 +90,71 @@ def read_all_garden():
 
 
 @app.post("/garden/update")
-def update_garden():
-    collection.update_one({"name": "moisture"}, {"$set": {"is_active": False}})
-    return {"message": "All garden is inactive."}
+def update_garden(data: Data = Body()):
+    result = list(collection.find({}, {"_id": False}))
+    for i in result:
+        if i["name"] == "sprinkle":
+            is_auto_1 = collection.find_one({"name": "sprinkle"}, {"_id": False})["sprinkle_1"]["is_auto"]
+            is_auto_2 = collection.find_one({"name": "sprinkle"}, {"_id": False})["sprinkle_2"]["is_auto"]
+            is_active_1 = collection.find_one({"name": "sprinkle"}, {"_id": False})["sprinkle_1"]["is_activate"]
+            is_active_2 = collection.find_one({"name": "sprinkle"}, {"_id": False})["sprinkle_2"]["is_activate"]
+            if is_auto_1 == True and is_active_1 == True:
+                collection.update_one({"name": "moisture"}, {"$set": {"moist_default": percentage_to_moisture((data.moist_default)), "moist_value": percentage_to_moisture((data.moist_value))}})
+            elif is_auto_1 == False and is_active_1 == False:
+                collection.update_one({"name": "moisture"}, {"$set": {"moist_default": percentage_to_moisture((data.moist_default)), "moist_value": percentage_to_moisture((data.moist_value))}})
+            elif is_active_1 == True:
+                collection.update_one({"name": "moisture"}, {"$set": {"moist_default":  percentage_to_moisture((data.moist_default)), "moist_value": percentage_to_moisture((data.moist_value))}})
+            elif is_auto_2 == True and is_active_2 == True:
+                collection.update_one({"name": "moisture"}, {"$set": {"moist_default": percentage_to_moisture((data.moist_default)), "moist_value": percentage_to_moisture((data.moist_value))}})
+            elif is_auto_2 == False and is_active_2 == False:
+                collection.update_one({"name": "moisture"}, {"$set": {"moist_default": percentage_to_moisture((data.moist_default)), "moist_value": percentage_to_moisture((data.moist_value))}})
+            elif is_active_2 == True:
+                collection.update_one({"name": "moisture"}, {"$set": {"moist_default":  percentage_to_moisture((data.moist_default)), "moist_value": percentage_to_moisture((data.moist_value))}})
+
+        if i["name"] == "moisture":
+            moisture = i["moist_value"]
+            moisture_default = i["moist_default"]
+            is_auto_1 = collection.find_one({"name": "sprinkle"}, {"_id": False})["sprinkle_1"]["is_auto"]
+            is_auto_2 = collection.find_one({"name": "sprinkle"}, {"_id": False})["sprinkle_2"]["is_auto"]
+            is_active_1 = collection.find_one({"name": "sprinkle"}, {"_id": False})["sprinkle_1"]["is_activate"]
+            is_active_2 = collection.find_one({"name": "sprinkle"}, {"_id": False})["sprinkle_2"]["is_activate"]
+            if moisture_default - moisture >= 100 and is_active_1 == True:
+                collection.update_one({"name": "sprinkle"}, {"$set": {"sprinkle_1":{"is_activate": True, "is_auto": False}}})
+            elif moisture_default - moisture < 100 and is_active_1 == True:
+                collection.update_one({"name": "sprinkle"}, {"$set": {"sprinkle_1":{"is_activate": True, "is_auto": False}}}) 
+            elif moisture_default - moisture >= 100 and is_auto_1 == True:
+                collection.update_one({"name": "sprinkle"}, {"$set": {"sprinkle_1":{"is_activate": True, "is_auto": False}}})
+            elif moisture_default - moisture < 100 and is_auto_1 == True:
+                collection.update_one({"name": "sprinkle"}, {"$set": {"sprinkle_1":{"is_activate": False, "is_auto": False}}})
+            elif moisture_default - moisture >= 100 and is_active_2 == True:
+                collection.update_one({"name": "sprinkle"}, {"$set": {"sprinkle_2":{"is_activate": True, "is_auto": False}}})
+            elif moisture_default - moisture < 100 and is_active_2 == True:
+                collection.update_one({"name": "sprinkle"}, {"$set": {"sprinkle_2":{"is_activate": True, "is_auto": False}}})
+            elif moisture_default - moisture >= 100 and is_auto_2 == True:
+                collection.update_one({"name": "sprinkle"}, {"$set": {"sprinkle_2":{"is_activate": True, "is_auto": False}}})
+            elif moisture_default - moisture < 100 and is_auto_2 == True:
+                collection.update_one({"name": "sprinkle"}, {"$set": {"sprinkle_2":{"is_activate": False, "is_auto": False}}})
+            elif moisture_default - moisture >= 100 and is_active_2 == True:
+                collection.update_one({"name": "sprinkle"}, {"$set": {"sprinkle_2":{"is_activate": False, "is_auto": False}}})
+            else:
+                collection.update_one({"name": "sprinkle"}, {"$set": {"sprinkle_1":{"is_activate": False, "is_auto": False}, 
+                "sprinkle_2":{"is_activate": False, "is_auto": False}}})
+                
+
+        elif i["name"] == "buzzer-sunroof":
+            buzzer = i["buzzer"]
+            sunroof = i["sunroof"]
+            if buzzer == True:
+                collection.update_one({"name":"buzzer-sunroof"}, {"$set":{"buzzer": True, "sunroof": False}})
+            elif buzzer == False:
+                collection.update_one({"name":"buzzer-sunroof"}, {"$set":{"buzzer": False}})
+            elif sunroof == True:
+                collection.update_one({"name":"buzzer-sunroof"}, {"$set":{"sunroof": True}})
+            elif sunroof == False:
+                collection.update_one({"name":"buzzer-sunroof"}, {"$set":{"sunroof": False}})
+
+    return {"message": "Garden is already update."}
+
 
 
 @app.get("/garden/sprinkle")
